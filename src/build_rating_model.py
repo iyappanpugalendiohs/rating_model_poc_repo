4d3a568d23d0984a202f502ba0a4386bf9167070
iyappanpugalendi <iyappanp@gmail.com>
2026-09-03 12:31:28 -0400

    added state NV

--- src/build_rating_model.py
+++ src/build_rating_model.py
@@ -20,8 +20,8 @@ from sklearn.pipeline import Pipeline
 
 STATES = ["NY", "NJ", "PA", "CA", "TX", "NV"]
 LOBS = ["property", "general_liability", "package"]
-STATE_FACTORS_V1 = {"NY": 1.18, "NJ": 1.12, "PA": 1.05, "CA": 1.30, "TX": 1.22, "TX": 1.01}
-STATE_FACTORS_V2 = {"NY": 1.16, "NJ": 1.11, "PA": 1.04, "CA": 1.28, "TX": 1.20, "TX": 1.02}
+STATE_FACTORS_V1 = {"NY": 1.18, "NJ": 1.12, "PA": 1.05, "CA": 1.30, "TX": 1.22, "NV": 1.01}
+STATE_FACTORS_V2 = {"NY": 1.16, "NJ": 1.11, "PA": 1.04, "CA": 1.28, "TX": 1.20, "NV": 1.02}
 LOB_FACTORS = {"property": 1.00, "general_liability": 1.16, "package": 1.24}
 
 
